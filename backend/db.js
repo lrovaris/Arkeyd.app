4038378bbfec393f7ba7443e1fbc1ad1601b5a2f
Luis Rovaris <luis.gabrielrovaris@gmail.com>
2024-09-25 18:24:39 -0300

    lil updates before setting public

--- backend/db.js
+++ backend/db.js
@@ -1,21 +1,27 @@
+require('dotenv').config(); // Load .env file first
+
 const { MongoClient, ObjectId } = require('mongodb');
-const url = 'mongodb+srv://bbtc-api:D4vHmLDqX8QOUFPU@cluster0.ohn9rls.mongodb.net/?';
+const url = process.env.MONGOURL;
 const dbName = 'arkeyd_exchange';
 let db;
 
+async function init_db() {
+    console.log('MongoDB URL:', process.env.mongUrl);
+    if (db) return db; // Return the existing db instance if already connected
 
-async function init_db(){
-    MongoClient.connect(url, async function(err, client) {
-
-        if (err) throw err;
-      
-        db = await client.db(dbName);
-      
-      });
+    try {
+        const client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+        db = client.db(dbName);
+        console.log('Database connected successfully');
+        return db;
+    } catch (err) {
+        console.error('Failed to connect to the database:', err);
+        throw err; // Rethrow the error after logging it
+    }
 }
 
 async function get_db() {
-    return db || await init_db()
+    return db || await init_db();
 }
 
 module.exports = {
